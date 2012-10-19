dc25f42826c9a5a6413dcdaf18373d203eb72730
Alex Dallaway [work] <alex.dallaway@gmail.com>
2012-10-19 14:21:38 -0400

    Added setAssociations to BaseEntity to handle nested record saves

--- models/BaseEntity.php
+++ models/BaseEntity.php
@@ -198,11 +198,114 @@ abstract class BaseEntity extends \lithium\data\Entity implements IModel {
 			}
 			$method = 'set' . Inflector::camelize($field);
 			if (method_exists($this, $method) && is_callable(array($this, $method))) {
-				$this->{$method}($value);
+			  $this->{$method}($value);
 			} else if (property_exists($this, $field)) {
-				$this->{$field} = $value;
+			  $this->{$field} = $value;
 			}
 		}
+	}
+
+	/**
+	 * Allows several properties to be assigned at once, i.e.:
+	 * {{{
+	 * $record->set(array('title' => 'Lorem Ipsum', 'value' => 42));
+	 * }}}
+	 *
+	 * @author alex.dallaway@gmail.com
+	 * @see IModel::validates()
+	 * @param array $data An associative array of fields and values to assign to this instance.
+	 * @throws Exception
+	 */
+	public function setAssociations(array $data)
+	{
+	  $entity_associations = $this->_getEntityAssociations();
+	  foreach($data as $association => $association_fields)
+	  {
+	    // Verify the association exists on this object and the value provided is an array:
+	    if(!is_string($association) || !array_key_exists($association, $entity_associations) || empty($association_fields)) continue;
+
+	    // We are dealing with a collection:
+	    if($entity_associations[$association]['is_collection'])
+	    {
+	      $collection_items = array();
+	      foreach($association_fields as $key => $collection_item)
+	      {
+	        /*
+	         * Workaround for when only the id has been submitted and is empty:
+	        */
+	        if((count($association_fields) == 1) && isset($association_fields['id']) && empty($association_fields['id']))
+	        {
+	          continue;
+	        }
+	        
+	        $target_class = isset($collection_item['__record_type']) ? $collection_item['__record_type'] : $entity_associations[$association]['target_class'];
+	        if(isset($collection_item['id']))
+	        {
+	          $find_result = static::getEntityManager()->find($target_class, $collection_item['id']);
+	          $collection_item_object = $find_result ?: new $target_class();
+	        }
+	        else
+	        {
+	          $collection_item_object = new $target_class();
+	        }
+	        $collection_item_object->set($collection_item, array(), false);
+	        $collection_item_object->setAssociations($collection_item);
+	        $collection_items[] = $collection_item_object;
+	      }
+	      $record_object = new \Doctrine\Common\Collections\ArrayCollection($collection_items);
+	    }
+	    
+	    // We are dealing with a single item:
+	    else
+	    {
+	      /*
+	       * Workaround for when only the id has been submitted and is empty:
+	       */
+	      if((count($association_fields) == 1) && isset($association_fields['id']) && empty($association_fields['id']))
+	      {
+	        continue;
+	      }
+	      
+  	    $target_class = isset($association_fields['__record_type']) ? $association_fields['__record_type'] : $entity_associations[$association]['target_class'];
+  	    
+  	    if(isset($association_fields['id']))
+  	    {
+  	      $find_result = static::getEntityManager()->find($target_class, $association_fields['id']);
+  	      $record_object = $find_result ?: new $target_class();
+  	    }
+  	    else
+  	    {
+  	      $record_object = new $target_class();
+  	    }
+  	    
+  	    $record_object->set($association_fields, array(), false);
+  	    $record_object->setAssociations($association_fields);
+	    }
+	    
+	    $set_method = 'set'.Inflector::camelize($association);
+	    if(method_exists($this, $set_method) && is_callable(array($this, $set_method)))
+	    {
+	      $this->{$set_method}($record_object);
+	    }
+	    else if(property_exists($this, $association))
+	    {
+	      $this->{$association} = $record_object;
+	    }
+	    
+	    // Don't try to persist the collections:
+	    if(get_class($record_object) != 'Doctrine\Common\Collections\ArrayCollection')
+	    {
+  	    $get_method = 'get'.Inflector::camelize($association);
+  	    if(method_exists($this, $get_method) && is_callable(array($this, $get_method)))
+  	    {
+  	      static::getEntityManager()->persist($this->{$get_method}());
+  	    }
+  	    else if(property_exists($this, $association))
+  	    {
+  	      static::getEntityManager()->persist($this->{$association});
+  	    }
+	    }
+	  }
 	}
 
 	/**
@@ -214,19 +317,28 @@ abstract class BaseEntity extends \lithium\data\Entity implements IModel {
 	 * @param bool $allProperties If true, get also properties without getter methods
 	 * @return mixed Entire data array if $name is empty, otherwise the value from the named field.
 	 */
-	public function data($name = null, $allProperties = false) {
+	public function data($name = null, $allProperties = false)
+	{
 		$data = $this->_getData($allProperties);
-		if (isset($name)) {
+		if(isset($name))
+		{
 		  if(strpos($name, '.'))
-      {
         return $this->_getNested($name);
-      }
       else
 			  return array_key_exists($name, $data) ? $data[$name] : null;
 		}
 		return $data;
 	}
   
+  /**
+   * Gets a nested field value of this object.
+   * 
+   * Used by data() to retrieve nested form field values for the Lithium Form helper.
+   * 
+   * @author alex.dallaway@gmail.com
+   * @return mixed The value of the nested field or null if it doesn't exist.
+   * @todo Rewrite this function to traverse as far as needed and handle collections.
+   */
   protected function _getNested($name)
   {
     $current = $this;
@@ -252,6 +364,20 @@ abstract class BaseEntity extends \lithium\data\Entity implements IModel {
     
     return null;
   }
+  
+  /**
+   * Gets a string representation of this object. This is used for record list display.
+   * 
+   * This should be overridden in any objects that extend this class to return more than the ID.
+   * (For example 'Brand' would return $this->getId().' '.$this->getName())
+   * 
+   * @author alex.dallaway@gmail.com
+   * @return string A string representation of this object.
+   */
+  public function toString()
+  {
+    return $this->getId();
+  }
 
 	/**
 	 * Get the entity fields
@@ -267,6 +393,30 @@ abstract class BaseEntity extends \lithium\data\Entity implements IModel {
 			}
 		}
 		return $entityFields;
+	}
+	
+	/**
+	 * Get the entity associations
+	 *
+	 * @author alex.dallaway@gmail.com
+	 * @return array
+	 */
+	protected function _getEntityAssociations()
+	{
+	  static $entity_associations;
+	  if(!isset($entity_associations))
+	  {
+	    $meta_data = static::getEntityManager()->getClassMetadata(get_called_class());
+	    $association_mappings = $meta_data->getAssociationMappings();
+	    $entity_associations = array();
+	    foreach($association_mappings as $field => $mapping_data)
+	    {
+	      $entity_associations[$field] = array();
+	      $entity_associations[$field]['target_class'] = $meta_data->getAssociationTargetClass($field);
+	      $entity_associations[$field]['is_collection'] = $meta_data->isCollectionValuedAssociation($field);
+	    }
+	  }
+	  return $entity_associations;
 	}
 
 	/**
